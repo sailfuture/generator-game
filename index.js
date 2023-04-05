0aa1bed80b12ab5562bff9a6b01b8ac37caaac34
sailfuture <78326976+sailfuture@users.noreply.github.com>
2023-04-05 07:29:43 -0400

    Update

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 // Define word lists
 const creditscore = ["600", "650", "700", "750", "800", "850"];
+const loanterm = ["15 years", "20 years", "30 years", "40 years"];
+const downpayment = ["5%", "10%", "15%", "20%", "25%"];
 
 // Pick a random word from a list
 const pickRandomWord = (listOfWords) => {
@@ -13,15 +15,23 @@ const pickRandomWord = (listOfWords) => {
 function generateRandomPhrase() {
   return {
     creditscore: pickRandomWord(creditscore),
+    loanterm: pickRandomWord(loanterm),
+    downpayment: pickRandomWord(downpayment),
   };
 }
 
 // Display a random phrase on the page
 function displayPhrase() {
-  const { creditscore} = generateRandomPhrase();
+  const { creditscore, loanterm, downpayment } = generateRandomPhrase();
 
   const creditscoreElem = document.querySelector(".creditscore");
   creditscoreElem.innerText = creditscore;
+
+  const loantermElem = document.querySelector(".loanterm");
+  loantermElem.innerText = loanterm;
+
+  const downpaymentElem = document.querySelector(".downpayment");
+  downpaymentElem.innerText = downpayment;
 }
 
 // Set up event listener on button
